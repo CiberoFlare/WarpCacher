15156960c0f4fa82e69fd328f61c222f318d0cc0
CiberoFlare <67067851+ciberoflare-official@users.noreply.github.com>
2020-07-12 15:30:17 +0800

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<?php include "sample.php" ?>
+<!DOCTYPE html>
+ <html>
+  <head>
+   echo "<title>";
+   echo "Sample Cache";
+   echo "</title>"
+  </head>
+  <body>
+    echo "Hi! I am just an example script to say it works";
+    echo "To know if it really works, refresh me, and try to view source code.";
+    echo "You will then see, a commented tag with a date on it";
+  </body>
+ </html>
