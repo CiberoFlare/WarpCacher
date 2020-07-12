1b07e9aed927cfa7a65e32dcd4d3054e6adbd6fd
CiberoFlare <67067851+ciberoflare-official@users.noreply.github.com>
2020-07-12 15:20:20 +0800

    Create warpcacher-start.php

---
+++ warpcacher-start.php
@@ -0,0 +1,18 @@
+<?php
+/* WarpCache Start
+$url = $_SERVER["SCRIPT_NAME"];
+$break = Explode('/', $url);
+$file = $break[count($break) - 1];
+$cachefile = 'cached-'.substr_replace($file ,"",-4).'.html';
+$cachetime = 18000;
+
+if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
+    echo "<!-- Cached copy, generated ".date('H:i', filemtime($cachefile))." -->\n";
+    readfile($cachefile);
+    exit;
+}
+
+ob_start();
+
+
+?>
