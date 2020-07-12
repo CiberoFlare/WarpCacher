c7e21a57ec2b8f6bf7d87db1a683f57f8239b6c4
SolarFlarion <67067851+SolarFlarion@users.noreply.github.com>
2020-07-12 17:26:33 +0800

    Update sample.php

--- sample.php
+++ sample.php
@@ -2,13 +2,17 @@
 <!DOCTYPE html>
  <html>
   <head>
+<?php
    echo "<title>";
    echo "Sample Cache";
    echo "</title>"
+?>
   </head>
   <body>
+<?php
     echo "Hi! I am just an example script to say it works";
     echo "To know if it really works, refresh me, and try to view source code.";
     echo "You will then see, a commented tag with a date on it";
-  </body>
+?>
+</body>
  </html>
